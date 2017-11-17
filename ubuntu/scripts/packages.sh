2f37f6c7199014d9ea77c3da3ab49a2e75b5a834
Jordo <me@jordanjtidwell.com>
2017-11-17 09:16:21 -0800

    added packages bash script.

---
+++ ubuntu/scripts/packages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -eux
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo ufw allow 'Nginx HTTP'
+sudo apt-get install -y git-all
+sudo apt-get install -y build-essential
+sudo apt-get install -y nodejs
+npm install -g typescript
+npm install -g gulp-cli 
+sudo apt-get install -y ruby-full
+gem install -y sass
